47dd7cd8273fa9adcf6c965934609c23be22a827
hawav <hwj101@163.com>
2020-05-10 09:35:52 +0000

    hello world test

--- api/getComics.ts
+++ api/getComics.ts
@@ -1,6 +1,6 @@
 import { NowRequest, NowResponse } from "@now/node";
 
 export default function (req: NowRequest, res: NowResponse) {
-  res.setHeader("Location", "https://www.baidu.com");
+  res.setHeader("Location", "https://www.zymk.cn/nodeapi/comic/allComic");
   res.status(303).end();
 }
