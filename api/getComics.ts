6da4e0f33b54b6fdffff3e377daeb7fa2da23eda
hawav <hwj101@163.com>
2020-05-10 09:54:04 +0000

    hello world test

--- api/getComics.ts
+++ api/getComics.ts
@@ -3,5 +3,5 @@ import { NowRequest, NowResponse } from "@now/node";
 export default function (req: NowRequest, res: NowResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Location", "https://www.zymk.cn/nodeapi/comic/allComic");
-  res.status(303).end();
+  res.status(302).end();
 }
